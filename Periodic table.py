b61390415f096698e40129de662cafb4d648328b
Gaurav <71424323+GauravKavat@users.noreply.github.com>
2020-09-26 12:31:57 +0530

    Add files via upload

---
+++ Periodic table.py
@@ -0,0 +1,621 @@
+import time
+
+a = int(input("Enter the atomic number of the  element : "))
+
+if a == 1:
+	print("Hydrogen is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 1.0078 amu")
+
+elif a == 2:
+	print("Helium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 4.0026 amu")
+
+elif a == 3:
+	print("Lithium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 6.938")
+
+elif a == 4:
+	print("Beryllium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 9.0122 amu")
+
+elif a == 5:
+	print("Boron is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 10.81 amu")
+
+elif a == 6:
+	print("Carbon is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 12.011 amu")
+
+elif a == 7:
+	print("Nitrogen is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 14.007 amu")
+
+elif a == 8:
+	print("Oxygen is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 15.999 amu")
+
+elif a == 9:
+	print("Florine is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 18.998 amu")
+
+elif a == 10:
+	print("Neon is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 20.180 amu")
+
+elif a == 11:
+	print("Sodium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 22.990 amu")
+
+elif a == 12:
+	print("Magnesium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 24.305 amu")
+
+elif a == 13:
+	print("Aluminium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 26.982 amu")
+
+elif a == 14:
+	print("Silicon is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 28.085 amu")
+
+elif a == 15:
+	print("Phosphorus is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 30.974 amu")
+
+elif a == 16:
+	print("Sulphur is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 32.06 amu")
+
+elif a == 17:
+	print("Chlorine is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 35.45 amu")
+
+elif a == 18:
+	print("Argon is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 39.948 amu")
+
+elif a == 19:
+	print("Potassium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 39.098 amu")
+
+elif a == 20:
+	print("Calcium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 40.078 amu")
+
+elif a == 21:
+	print("Scandium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 44.956 amu")
+
+elif a == 22:
+	print("Titanium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 47.867 amu")
+
+elif a == 23:
+	print("Vandium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 50.942 amu")
+
+elif a == 24:
+	print("Chromium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 51.996 amu")
+
+elif a == 25:
+	print("Magneese is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 54.938 amu")
+
+elif a == 26:
+	print("Iron is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 55.845 amu")
+
+elif a == 27:
+	print("Cobalt is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 58.933 amu")
+
+elif a == 28:
+	print("Nickel is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 58.693 amu")
+
+elif a == 29:
+	print("Copper is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 63.546 amu")
+
+elif a == 30:
+	print("Zinc is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 65.38 amu")
+
+elif a == 31:
+	print("Garium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 69.723 amu")
+
+elif a == 32:
+	print("Germanium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 72.630 amu")
+
+elif a == 33:
+	print("Arsenic is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 74.922 amu")
+
+elif a == 34:
+	print("Selenium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 78.971 amu")
+
+elif a == 35:
+	print("Bromine is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 79.904 amu")
+
+elif a == 36:
+	print("Krypton is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 83.798 amu")
+
+elif a == 37:
+	print("Rubidium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 85.468 amu")
+
+elif a == 38:
+	print("Strontium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 87.62 amu")
+
+elif a == 39:
+	print("Yttrium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 88.906 amu")
+
+elif a == 40:
+	print("Zirconium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 91.224 amu")
+
+elif a == 41:
+	print("Niobium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 92.906 amu")
+
+elif a == 42:
+	print("Molybdenum is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 95.96 amu")
+
+elif a == 43:
+	print("Technetium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 98.9062 amu")
+
+elif a == 44:
+	print("Ruthenium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 101.07 amu")
+
+elif a == 45:
+	print("Rhodium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 102.91 amu")
+
+elif a == 46:
+	print("Palladium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 106.42 amu")
+
+elif a == 47:
+	print("Silver is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 107.87 amu")
+
+elif a == 48:
+	print("Cadmium the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 112.41 amu")
+
+elif a == 49:
+	print("Indium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 114.82 amu")
+
+elif a == 50:
+	print("Tin is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 118.71 amu")
+
+elif a == 51:
+	print("Antimony is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 121.76 amu")
+
+elif a == 52:
+	print("Tellurium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 127.60 amu")
+
+elif a == 53:
+	print("Iodine is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 126.90 amu")
+
+elif a == 54:
+	print("Xenon is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 131.29 amu")
+
+elif a == 55:
+	print("Caesium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 132.91 amu")
+
+elif a == 56:
+	print("Barium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is Barium is ")
+
+elif a == 57:
+	print("Lanthanum is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 138.91 amu")
+
+elif a == 58:
+	print("Cerium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 140.12 amu")
+
+elif a == 59:
+	print("Praseodymium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 140.91 amu")
+
+elif a == 60:
+	print("Neodymium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 144.24 amu")
+
+elif a == 61:
+	print("Promethium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 145 amu")
+
+elif a == 62:
+	print("Samarium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 150.36 amu")
+
+elif a == 63:
+	print("Europium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 151.96 amu")
+
+elif a == 64:
+	print("Gadolinium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 157.25 amu")
+
+elif a == 65:
+	print("Terbium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 158.93 amu")
+
+elif a == 66:
+	print("Dysprosium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 162.50 amu")
+
+elif a == 67:
+	print("Holmium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 164.93 amu")
+
+elif a == 68:
+	print("Erbium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 167.26 amu")
+
+elif a == 69:
+	print("Thulium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 168.93 amu")
+
+elif a == 70:
+	print("Ytterbium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 173.04 amu")
+
+elif a == 71:
+	print("Lutetium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 174.97 amu")
+
+elif a == 72:
+	print("Hafnium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 178.49 amu")
+
+elif a == 73:
+	print("Tantalum is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 180.95 amu")
+
+elif a == 74:
+	print("Tungsten is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 183.84 amu")
+
+elif a == 75:
+	print("Rhenium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 186.21 amu")
+
+elif a == 76:
+	print("Osmium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 190.23 amu")
+
+elif a == 77:
+	print("Iridium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 192.22 amu")
+
+elif a == 78:
+	print("Platinum is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 195.08 amu")
+
+elif a == 79:
+	print("Gold is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 196.97 amu")
+
+elif a == 80:
+	print("Mercury is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 200.59 amu")
+
+elif a == 81:
+	print("Thallium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 204.38 amu")
+
+elif a == 82:
+	print("Lead is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 207.2 amu")
+
+elif a == 83:
+	print("Bismuth is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 208.98 amu")
+
+elif a == 84:
+	print("Polonium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 209 amu")
+
+elif a == 85:
+	print("Astatine is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 210 amu")
+
+elif a == 86:
+	print("Radon is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 222 amu")
+
+elif a == 87:
+	print("Francium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 223 amu")
+
+elif a == 88:
+	print("Radium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 226 amu")
+
+elif a == 89:
+	print("Actinium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 227 amu")
+
+elif a == 90:
+	print("Thorium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 232.04 amu")
+
+elif a == 91:
+	print("Protactinium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 231.04 amu")
+
+elif a == 92:
+	print("Uranium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 238.04 amu")
+
+elif a == 93:
+	print("Neptunium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 237 amu")
+
+elif a == 94:
+	print("Plutonium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 244 amu")
+
+elif a == 95:
+	print("Americium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 243 amu")
+
+elif a == 96:
+	print("Curium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 247 amu")
+
+elif a == 97:
+	print("Berkelium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 247 amu")
+
+elif a == 98:
+	print("Californium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 251 amu")
+
+elif a == 99:
+	print("Einsteinium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 252 amu")
+
+elif a == 100:
+	print("Fermium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 257 amu")
+
+elif a == 101:
+	print("Mendelevium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 258 amu")
+
+elif a == 102:
+	print("Nobelium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 259 amu")
+
+elif a == 103:
+	print("Lawrencium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 262 amu")
+
+elif a == 104:
+	print("Rutherfordium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 261 amu")
+
+elif a == 105:
+	print("Dubnium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 262 amu")
+
+elif a == 106:
+	print("Seaborgium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 266 amu")
+
+elif a == 107:
+	print("Bhorium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 264 amu")
+
+elif a == 108:
+	print("Hassium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 269 amu")
+
+elif a == 109:
+	print("Meitnerium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 268 amu")
+
+elif a == 110:
+	print("Darmstadtium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 281 amu")
+
+elif a == 111:
+	print("Roentgenium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 272 amu")
+
+elif a == 112:
+	print("Copernicium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 285 amu")
+
+elif a == 113:
+	print("Nihonium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 286 amu")
+
+elif a == 114:
+	print("Flerovium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 289 amu")
+
+elif a == 115:
+	print("Moscovium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 288 amu")
+
+elif a == 116:
+	print("Livermorium is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 292 amu")
+
+elif a == 117:
+	print("Tennessine is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 117 amu")
+
+elif a == 118:
+	print("Oganesson is the element with",a ,"atomic number")
+	time.sleep(0.5)
+	print("The atomic mass is 294 amu")
+
+else :
+	print("No such element exists: IF YOU NEED ONE !! GO FIND ONE !!!")
+	time.sleep(0.5)
+	print("But choose a easy name for it !!")
+
+time.sleep(1)
+print("Thank you")
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+print()
+input("Press ENTER to close")
